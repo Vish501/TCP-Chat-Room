216f98065eaf7f152a9e136bd3dfe8a29c01d8b3
Vish501 <76504332+Vish501@users.noreply.github.com>
2025-01-07 12:23:41 +0000

    Implementing client side code

--- client.py
+++ client.py
@@ -2,22 +2,43 @@ from datetime import datetime
 import threading
 import socket
 
+RECV_SIZE = 1024
+JOINED_MESSAGE = 'JOINED_MESSAGE'
 
 # Connecting to the client
 def receive():
-    pass
+    while True:
+        try:
+            message = client.recv().decode('ascii')
+            if message == JOINED_MESSAGE:
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print('An error occured! Closing Connection...')
+            client.close()
+            break
 
 
 # Writing a message to be broadcasted by the server
 def write_messsage():
-    pass
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('ascii'))
 
 
 
 if __name__ == '__main__':
     host = input(f'Enter the host you want to connect to: ')
     port = int(input(f'Enter the port you want to connect to: '))
+    nickname = input(f'Enter the nickname you want to use: ')
 
     print('Trying to initialize connection...')
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     client.connect((host, port))
+
+    receive_thread = threading.Thread(target=receive)
+    receive_thread.start()
+
+    write_thread = threading.Thread(target=write_messsage)
+    write_thread.start()
